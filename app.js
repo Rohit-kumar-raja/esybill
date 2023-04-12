2127fc514d6e0dc905b2c2e1e56000eb66e6edd2
Suryans Goel <suryansgoel1998@gmail.com>
2023-04-12 22:19:10 +0530

    setup route

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
+const routes = require('./routes/index');
 
 if (process.env.NODE_ENV && process.env.NODE_ENV !== 'production') {
   dotenv.config();
@@ -7,9 +9,11 @@ if (process.env.NODE_ENV && process.env.NODE_ENV !== 'production') {
 
 const app = express();
 app.use(express.json());
-app.get('/api/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, './frontend/build')));
+
+app.use('/api', routes);
+
 app.listen(process.env.NODE_PORT || 4000, () => {
   // eslint-disable-next-line
   console.log('Server is running on port ', process.env.NODE_PORT || 4000);
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const userRoute = require('./route.user');
+
+router.use('/user', userRoute);
+module.exports = router;
