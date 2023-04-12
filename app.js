8eeb9b0a2c1e224d62b0fe2c172202afe032dca9
Suryans Goel <suryansgoel1998@gmail.com>
2023-04-12 21:33:43 +0530

    basic eslint setup

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
-if (process.env.NODE_ENV && process.env.NODE_ENV != 'production')
-  require('dotenv').config();
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV && process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
+
 const app = express();
 app.use(express.json());
 app.get('/api/', (req, res) => {
   res.send('Hello World');
 });
 app.listen(process.env.NODE_PORT || 4000, () => {
+  // eslint-disable-next-line
   console.log('Server is running on port ', process.env.NODE_PORT || 4000);
 });
