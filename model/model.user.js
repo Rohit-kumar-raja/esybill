24dd7e63b78c933a6a88305e92fc1f7f359ef2cd
Suryans Goel <suryansgoel1998@gmail.com>
2023-04-18 23:29:57 +0530

    SMS

--- model/model.user.js
+++ model/model.user.js
@@ -1,18 +1,18 @@
 const db = require('../lib/db');
 
-async function getUser() {
-  const connection = await db();
-  const users = await connection.execute('SELECT * FROM tblmastercustomer');
-  return users[0];
-}
-
 async function getUserByPhone(phone) {
   const connection = await db();
   const users = await connection.execute('SELECT * FROM tblmastercustomer WHERE RegMobile=?', [phone]);
   return users[0];
 }
 
+async function updateUser(user, phone) {
+  const connection = await db();
+  const users = await connection.execute('UPDATE ezydiner.tblmastercustomer SET CustomerName=?, RegMobile=?, RegEmail=NULL, State=?, Country=? WHERE RegMobile=?', [phone]);
+  return users[0];
+}
+
 module.exports = {
-  getUser,
-  getUserByPhone
+  getUserByPhone,
+  updateUser
 };
