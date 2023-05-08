820a77e9c2d447d9bdff4581b1da5bc8a0bc7e41
Suryans Goel <suryansgoel1998@gmail.com>
2023-05-08 23:51:17 +0530

    setting up email(incomplete)

--- util/mail.js
+++ util/mail.js
@@ -1,5 +1,29 @@
-async function sendMail() {
-  return true;
+const nodeMailer = require('nodemailer');
+
+const transporter = nodeMailer.createTransport({
+  host: 'smtp.zoho.com',
+  secure: true,
+  port: 465,
+  auth: {
+    user: 'email@domain.is',
+    pass: 'password_here'
+  }
+});
+function sendMail(email, subject, body) {
+  return new Promise((resolve, reject) => {
+    const mailOptions = {
+      from: 'email@domain.com',
+      to: email,
+      subject,
+      html: body
+    };
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve();
+    });
+  });
 }
 
 module.exports = { sendMail };
