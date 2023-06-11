a8b976b4da6612300e7eca20e100e14d1e2303c4
Suryans Goel <suryansgoel1998@gmail.com>
2023-06-11 23:46:33 +0530

    multer

--- backend/test.js
+++ backend/test.js
@@ -1,24 +1,3 @@
-// /* eslint-disable */
-const fs = require('fs');
+const config = require('./config/config');
 
-fs.readFile('./app.js', (err, data) => {
-  console.log('Start');
-
-  setImmediate(() => {
-    console.log('setImmediate');
-  });
-
-  // Promise.resolve().then(() => {
-  //   console.log('Promise');
-  // });
-
-  process.nextTick(() => {
-    console.log('nextTick');
-  });
-
-  // setTimeout(() => {
-  //   console.log('setTimeout');
-  // }, 0);
-
-  console.log('End');
-});
+console.log(config.MenuImages);
