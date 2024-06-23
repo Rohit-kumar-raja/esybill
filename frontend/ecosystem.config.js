cdd9da92b2beb791606349eac256ee18246a6260
Raja <raja@raja.com>
2024-06-23 23:46:58 +0530

    akkd oe

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         port: '3001',
         exec_mode: 'cluster',
         instances: 'max',
-        script: './.output/server/index.mjs'
+        script: 'serve build/'
       }
     ]
   }
